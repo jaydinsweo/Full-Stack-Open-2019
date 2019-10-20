62358a6a4008d923d5e85ed264d412d56b19b33d
Jay Nguyen <iam.nguyen@pm.me>
2019-10-20 16:09:14 +0100

    1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,13 +4,22 @@ const Button = ({ onClick, text }) => (
 	<button onClick={onClick}> {text}</button>
 );
 
-const Statistics = ({ good, neutral, bad }) => (
-	<div>
-		<p> all {good + neutral + bad}</p>
-		<p> average {(good - bad) / (good + neutral + bad)}</p>
-		<p>positive {(good / (good + neutral + bad)) * 100}%</p>
-	</div>
-);
+const Statistics = ({ good, neutral, bad }) => {
+	if (good + neutral + bad === 0) {
+		return <p>No feedback given</p>;
+	}
+	return (
+		<div>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+
+			<p> all {good + neutral + bad}</p>
+			<p> average {(good - bad) / (good + neutral + bad)}</p>
+			<p>positive {(good / (good + neutral + bad)) * 100}%</p>
+		</div>
+	);
+};
 
 function App() {
 	const [good, setGood] = useState(0);
@@ -24,9 +33,6 @@ function App() {
 			<Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
 			<Button onClick={() => setBad(bad + 1)} text={'bad'} />
 			<h2> statistics </h2>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
 			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	);
