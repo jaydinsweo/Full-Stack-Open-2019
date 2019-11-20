5734abf9f329df7503e0a1c24d8d58d88851c6af
nguyenami <iam.nguyen@pm.me>
2019-11-20 19:16:01 +0000

    4.10

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -7,6 +7,14 @@ Router.get("/", (req, res, next) => {
     res.json(blogs.map(blog => blog.toJSON()));
   });
 });
+
+Router.post("/", async (req, res, next) => {
+  const blog = new Blog(req.body);
+
+  await blog.save();
+  res.status(201).json(blog.save());
+});
+
  */
 
 Router.get("/", async (req, res, next) => {
@@ -15,10 +23,22 @@ Router.get("/", async (req, res, next) => {
 });
 
 Router.post("/", async (req, res, next) => {
-  const blog = new Blog(req.body);
+  const body = req.body;
 
-  await blog.save();
-  res.status(201).json(blog.save());
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  });
+
+  try {
+    const savedBlog = await blog.save();
+    res.status(201);
+    res.json(savedBlog.toJSON());
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = Router;
--- part4/test/node_api.test.js
+++ part4/test/node_api.test.js
@@ -30,3 +30,20 @@ it("return a specific blog in the database", async () => {
   const content = respone.body.map(blog => blog.title);
   expect(content).toContain("Hello");
 });
+
+describe("test http post request", () => {
+  it("test obj can be added", async () => {
+    await api
+      .post("/api/blog")
+      .send(initBlog[0])
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const respone = await api.get("/api/blog");
+
+    const contents = respone.body.map(r => r.title);
+
+    expect(respone.body.length).toBe(initBlog.length + 1);
+    expect(contents).toContain("Hello");
+  });
+});
